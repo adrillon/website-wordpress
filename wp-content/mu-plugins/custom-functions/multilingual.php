78c965cbc4dc98057a55eddb5a1408d378ff3d6c
Alain DRILLON <contact@drillon-ala.in>
2019-02-10 15:50:46 +0100

    Add multilingual support in REST API

---
+++ wp-content/mu-plugins/custom-functions/multilingual.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Whenever a post (of any type) is queried via the REST API, the ID of the matching post in each language is added to the result.
+ */
+
+add_action('init', 'multilingual_wp_init');
+function multilingual_wp_init() {
+    foreach (get_post_types([
+        'public' => true,
+    ]) as $postType) {
+        add_filter('rest_prepare_' . $postType, 'add_link_to_translations', 10, 3);
+    }
+}
+
+function add_link_to_translations($response, $post, $request) {
+    $mlp_language_api = apply_filters('mlp_language_api', NULL);
+    if ($mlp_language_api === NULL) return $response; // do nothing if Multilingual Press is not enabled
+
+    $translations = $mlp_language_api->get_translations([
+        'content_id' => $post->ID,
+        'include_base' => true,
+        'type' => 'post',
+    ]);
+    $response->data['translations'] = array();
+    foreach ($translations as $t) {
+        $response->data['translations'][$t->get_language()->get_name('lang')] = $t->get_target_content_id();
+    }
+    return $response;
+}
+
+
+/**
+ * Add a custom REST route to get the list of all languages with their URL.
+ */
+
+add_action('rest_api_init', function() {
+    $mlp_language_api = apply_filters('mlp_language_api', NULL);
+    if ($mlp_language_api === NULL) return $response; // do nothing if Multilingual Press is not enabled
+
+    register_rest_route(
+        'custom/v1',
+        'langs',
+        [
+            'method' => 'GET',
+            'callback' => __NAMESPACE__.'\custom_get_langs',
+        ]
+    );
+});
+
+function custom_get_langs($request) {
+    $mlp_language_api = apply_filters('mlp_language_api', NULL);
+
+    $sites = array();
+    foreach (get_sites() as $s) {
+        $sites[] = [
+            'url' => get_home_url($s->blog_id),
+            'lang' => mlp_get_blog_language($s->blog_id),
+        ];
+    }
+    return $sites;
+}
